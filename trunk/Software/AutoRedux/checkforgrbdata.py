d890dd3beccb7225790b02e3b4efe31ccb1c95cc
amorgan <amorgan@15f48517-4a6d-0410-94cc-8a7d56af1ecd>
2009-08-18 18:23:20 +0000

    Minor updates  to checkforgrbdata; path stuff, etc.  still needs more development to be operational within q_soft
    git-svn-id: svn+ssh://lyra.berkeley.edu/Bloom/amorgan/SVNREPO/q_soft@31 15f48517-4a6d-0410-94cc-8a7d56af1ecd

--- trunk/Software/AutoRedux/checkforgrbdata.py
+++ trunk/Software/AutoRedux/checkforgrbdata.py
@@ -13,20 +13,23 @@ import glob
 import shutil
 import cPickle as pickle 
 
-### DETERMINE OR CREATE TEMPORARY AUTOREDUX DIRECTORY
-cronpath = os.path.expanduser("~/ptelautoredux/")
-if not os.path.exists(cronpath):  #create storage directory if it doesn't exist already
-    try:
-	os.system("mkdir " + cronpath)
-	os.system("mkdir " + cronpath + "tmp")
-	print "Creating Temporary directory %s and moving files there." % (cronpath)
-    except:
-	print "CANNOT MAKE TEMP DIRECTORY.  Will try to continue anyway, but will likely fail."
-	pass
-else: print "Copying temporary files to %s" % (cronpath)
+req_num_of_files = 5
 
-pklflagpath = cronpath+'ptelgrbcheck.pkl'
-reduxdir = cronpath+'/tmp/'
+### DETERMINE OR CREATE TEMPORARY AUTOREDUX DIRECTORY
+autoreduxdir = os.path.expanduser("~/ptel_auto_redux/")
+if not os.path.exists(autoreduxdir):  #create storage directory if it doesn't exist already
+    try:
+        os.system("mkdir " + autoreduxdir)
+        os.system("mkdir " + autoreduxdir + "tmp")
+        print "Creating Temporary directory %s and moving files there." % (autoreduxdir)
+    except:
+        print "CANNOT MAKE TEMP DIRECTORY.  Will try to continue anyway, but will likely fail."
+        pass
+else: 
+    print "Copying temporary files to %s" % (autoreduxdir)
+
+pklflagpath = autoreduxdir+'ptelgrbcheck.pkl'
+reduxdir = autoreduxdir+'/tmp/'
 
 bodystring = ''
 errmsg = ''
@@ -42,77 +45,78 @@ if os.path.exists(searchpath):
     ### LOAD or CREATE PICKLE FLAG FILE 
     # The pickle file here contains flags about what has been reduced, etc
     if os.path.exists(pklflagpath): 
-	storefile=open(pklflagpath)
-	pklflagdict = pickle.load(storefile)
-	storefile.close()
+        storefile=open(pklflagpath)
+        pklflagdict = pickle.load(storefile)
+        storefile.close()
     else:
-	### Create an empty pickle file if it doesn't exist
-	storefile = open(pklflagpath,'w')
-	thedict = {'flag':0,'dateflag':0}
-	pklflagdict=thedict
-	pickle.dump(pklflagdict,storefile)
-	storefile.close
-	bodystring += "No Pickle file existed, so one was created\n\n"
+        ### Create an empty pickle file if it doesn't exist
+        storefile = open(pklflagpath,'w')
+        thedict = {'flag':0,'dateflag':0}
+        pklflagdict=thedict
+        pickle.dump(pklflagdict,storefile)
+        storefile.close
+        bodystring += "No Pickle file existed, so one was created\n\n"
 
-    ### DETERMINE NUMBER OF FILES IN DIRECTORY
-    globfiles = glob.glob(searchpath+searchstring)
-    numfiles = len(globfiles)
+        ### DETERMINE NUMBER OF FILES IN DIRECTORY
+        globfiles = glob.glob(searchpath+searchstring)
+        numfiles = len(globfiles)
     
-    ### START COMPOSITION OF EMAIL
-    bodystring +="There are now %s files in %s" % (str(numfiles), searchpath)
-    bodystring +='\n'
+        ### START COMPOSITION OF EMAIL
+        bodystring +="There are now %s files in %s" % (str(numfiles), searchpath)
+        bodystring +='\n'
 
-    ### DETERMINE IF CRITERIA TO TAKE ACTION ARE SATISFIED
-    ### Need X number of files, and for this to be a different date than before 
-    if (numfiles > 2) and (pklflagdict['dateflag'] != dateint): 
+        ### DETERMINE IF CRITERIA TO TAKE ACTION ARE SATISFIED
+        ### Need X number of files, and for this to be a different date than before 
+        if (numfiles > req_num_of_files) and (pklflagdict['dateflag'] != dateint): 
 
-	### MOVE FILES TO TEMPORARY PATH FOR REDUCTION
-	### Since files can't be reduced in their initial directory,
-	### Move to a location where the user has write permission 
-	if os.path.exists(reduxdir): 
-	    for i in range(0,numfiles):
-		shutil.copy(globfiles[i],reduxdir)
-	    bodystring +=  "\nCopied files to %s" % (reduxdir)
-	else: bodystring += "\nCould not move files, the path %s does not exist." % (reduxdir)
+        	### MOVE FILES TO TEMPORARY PATH FOR REDUCTION
+        	### Since files can't be reduced in their initial directory,
+        	### Move to a location where the user has write permission 
+        	if os.path.exists(reduxdir): 
+        	    for i in range(0,numfiles):
+                    shutil.copy(globfiles[i],reduxdir)
+                    bodystring +=  "\nCopied files to %s" % (reduxdir)
+        	else: bodystring += "\nCould not move files, the path %s does not exist." % (reduxdir)
 
-	### SEND EMAIL TO APPROPRIATE PEOPLE
-	try:  # Only do if the domail module exists
-	    import domail
-	    domail.domail('qmorgan@gmail.com','Files Updated',bodystring)  
-	except:
-	    pass 
+    	### SEND EMAIL TO APPROPRIATE PEOPLE
+    	try:  # Only do if the domail module exists
+    	    import domail
+    	    domail.domail('qmorgan@gmail.com','Files Updated',bodystring)  
+    	except:
+    	    print bodystring
+    	    print "Could not send email; no domail.py module found". 
 
-	### UPDATE DICTIONARY FLAGS
-	pklflagdict['dateflag'] = dateint #Only perform actions once per date
+    	### UPDATE DICTIONARY FLAGS
+    	pklflagdict['dateflag'] = dateint #Only perform actions once per date
 	
-    # Is another flag useful for something?
-    # Maybe have it be a init_is_moved or something, but need new pkl files for each 
-    if (numfiles > 4):   
-	pklflagdict['flag'] = 1
-    else: pklflagdict['flag'] = 0
+        # Is another flag useful for something?
+        # Maybe have it be a init_is_moved or something, but need new pkl files for each 
+        if (numfiles > 4):   
+    	    pklflagdict['flag'] = 1
+        else: pklflagdict['flag'] = 0
 
-    ### SAVE UPDATED INFO IN PICKLE FILE
-    storefile = open(pklflagpath,'w')
-    pickle.dump(pklflagdict,storefile)
-    storefile.close
+        ### SAVE UPDATED INFO IN PICKLE FILE
+        storefile = open(pklflagpath,'w')
+        pickle.dump(pklflagdict,storefile)
+        storefile.close
 
-### END
+    ### END
 
 ### This is just a definition to reset the flags in the flag dictionary for testing purposes 
 def resetflags():
     if os.path.exists(pklflagpath):
-	storefile = open(pklflagpath,'w')
-	pklflagdict = {'flag':0,'dateflag':0}
-	pickle.dump(pklflagdict,storefile)
-	storefile.close
+        storefile = open(pklflagpath,'w')
+	    pklflagdict = {'flag':0,'dateflag':0}
+	    pickle.dump(pklflagdict,storefile)
+	    storefile.close
 
 def determinesearchpath(sem="sem2009a",year=None,month=None,day=None):
 
     if (year == None) or (month == None) or (day == None):
-	tlist=time.gmtime()
-	year = tlist[0] ; month = tlist[1] ; day = tlist[2] 
+	    tlist=time.gmtime()
+	    year = tlist[0] ; month = tlist[1] ; day = tlist[2] 
 	print "Using current UT date as directory search path"
-    sem = "sem2009a"
-    datestr = time.strftime("%Y-%b-%d",(year,month,day,0,0,0,0,1,-1))
-    dirpath = "/Bloom/PAIRITEL-DATA/%s/Dir%s/" % (sem,datestr)
+        sem = "sem2009a"
+        datestr = time.strftime("%Y-%b-%d",(year,month,day,0,0,0,0,1,-1))
+        dirpath = "/Bloom/PAIRITEL-DATA/%s/Dir%s/" % (sem,datestr)
     return dirpath
