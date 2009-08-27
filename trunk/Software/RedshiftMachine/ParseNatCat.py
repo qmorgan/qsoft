8fe343ee5f779313d325b05fce57f200b47e611b
amorgan <amorgan@15f48517-4a6d-0410-94cc-8a7d56af1ecd>
2009-08-27 19:34:29 +0000

    Added "remove_zeros" option to remove the entries in the parsed nat dictionary that have '0.0' as a value, under the assumption that the true values are unknown or undefined.
    git-svn-id: svn+ssh://lyra.berkeley.edu/Bloom/amorgan/SVNREPO/q_soft@48 15f48517-4a6d-0410-94cc-8a7d56af1ecd

--- trunk/Software/RedshiftMachine/ParseNatCat.py
+++ trunk/Software/RedshiftMachine/ParseNatCat.py
@@ -9,7 +9,8 @@ Last Updated: Aug 26, 2009
 	
 This program loads the fits tables from Nat's website http://astro.berkeley.edu/~nat/ 
 as described in Butler et al. (2007; ApJ, 671, 656) and puts them in a dictionary.
-
+If remove_zeroes == True, then remove the dictionary entries under the assumption 
+that they were set to Zero in the catalog because the true value was unknown.
 
 """
 import cPickle as pickle
@@ -29,7 +30,7 @@ storepath = os.environ.get("Q_DIR") + '/store/'
 
 default_catlist = [storepath+'bat_catalog_07061275.fits',storepath+'bat_catalog_current.fits']
 
-def read_nat_bat_cat(fitsname):
+def read_nat_bat_cat(fitsname,remove_zeros=False):
     hdulist = pyfits.open(fitsname)
     tbdata = hdulist[1].data
     cols = hdulist[1].columns
@@ -39,13 +40,26 @@ def read_nat_bat_cat(fitsname):
     for i in range(len(tbdata)):
         subdict = dict(zip(colnames,tbdata[i]))
         grbname = subdict.pop('GRB') # remove GRBname from dictionary
+        # Had iteration problems trying to remove dictionary entries while
+        # iterating over them, so just populate a new dictionary with values
+        # that are NOT zero.
+        if remove_zeros == True:
+            subdict_zrem = {}
+            for key,val in subdict.iteritems():
+                if subdict[key] != 0.0:
+                    subdict_zrem.update({key:val})
+            subdict = subdict_zrem
         nat_dict.update({grbname:subdict})
     return nat_dict
 
-def combine_natcats(natcatlist=default_catlist):
+def combine_natcats(natcatlist=default_catlist,remove_zeros=False):
     comb_nat_dict = {}
     for natcat in natcatlist:
-        sub_nat_dict = read_nat_bat_cat(natcat)
+        sub_nat_dict = read_nat_bat_cat(natcat,remove_zeros)
         comb_nat_dict.update(sub_nat_dict)
+    # if remove_zeros = True:
+    #     for i in iter(comb_nat_cat):
+    #         for ii in iter(comb_nat_cat[i]):
+    #             if comb
     return comb_nat_dict
 
