6b6333d99015562d2d04e15a15f6cc4c7a1a8357
amorgan <amorgan@15f48517-4a6d-0410-94cc-8a7d56af1ecd>
2009-10-01 01:29:54 +0000

    Added GRAASP ToO link
    git-svn-id: svn+ssh://lyra.berkeley.edu/Bloom/amorgan/SVNREPO/q_soft@76 15f48517-4a6d-0410-94cc-8a7d56af1ecd

--- trunk/Software/RedshiftMachine/GRBHTML.py
+++ trunk/Software/RedshiftMachine/GRBHTML.py
@@ -13,6 +13,7 @@ import sys
 import os
 import shutil
 import time
+from MiscBin import q
 
 if not os.environ.has_key("Q_DIR"):
     print "You need to set the environment variable Q_DIR to point to the"
@@ -64,18 +65,34 @@ class GRBHTML:
         <hr width="50%">
         <b>Position Information:</b><p>
         '''
+        if bat_pos: 
+            bat_pos_sex = q.dec2sex((bat_pos[0],bat_pos[1]))
+            self.best_pos = bat_pos
+        if xrt_pos: 
+            xrt_pos_sex = q.dec2sex((xrt_pos[0],xrt_pos[1]))
+            self.best_pos = xrt_pos
+        if uvot_pos: 
+            uvot_pos_sex = q.dec2sex((uvot_pos[0],uvot_pos[1]))
+            self.best_pos = uvot_pos
+        
         if bat_pos != None:
             self.html_block += '''
             <b>BAT</b>: RA = %s, Dec = %s, Uncertainty: %s"<br>
-            ''' % (str(bat_pos[0]).rstrip('0'),str(bat_pos[1]).rstrip('0'),str(bat_pos[2]).rstrip('0'))
+            (RA = %s, Dec = %s)<br><br>
+            ''' % (str(bat_pos[0]).rstrip('0'),str(bat_pos[1]).rstrip('0'),str(bat_pos[2]).rstrip('0'),\
+                   bat_pos_sex[0],bat_pos_sex[1])
         if xrt_pos != None:
             self.html_block += '''
-            <b>XRT</b>: RA = %s, Dec = %s, Uncertainty: %s"<p>
-            ''' % (str(xrt_pos[0]).rstrip('0'),str(xrt_pos[1]).rstrip('0'),str(xrt_pos[2]).rstrip('0'))
+            <b>XRT</b>: RA = %s, Dec = %s, Uncertainty: %s"<br>
+            (RA = %s, Dec = %s)<br><br>
+            ''' % (str(xrt_pos[0]).rstrip('0'),str(xrt_pos[1]).rstrip('0'),str(xrt_pos[2]).rstrip('0'),\
+            xrt_pos_sex[0],xrt_pos_sex[1])
         if uvot_pos != None:
             self.html_block += '''
-            <b>XRT</b>: RA = %s, Dec = %s, Uncertainty: %s"<p>
-            ''' % (str(uvot_pos[0]).rstrip('0'),str(uvot_pos[1]).rstrip('0'),str(uvot_pos[2]).rstrip('0'))
+            <b>XRT</b>: RA = %s, Dec = %s, Uncertainty: %s"<br><br>
+            (RA = %s, Dec = %s)<br>
+            ''' % (str(uvot_pos[0]).rstrip('0'),str(uvot_pos[1]).rstrip('0'),str(uvot_pos[2]).rstrip('0'),\
+            uvot_pos_sex[0],uvot_pos_sex[1])
         if reg_path != None:
             if os.path.exists(reg_path):
                 self.copy_file(reg_path)
@@ -99,6 +116,16 @@ class GRBHTML:
         else:
             print reg_path + ' does not exist.  Not including Finder Chart.'
     
+    def add_telescope_info(self):
+        too_str = ''
+        if hasattr(self, 'best_pos'):
+            too_str = '?ra=%f&dec=%f' % (self.best_pos[0],self.best_pos[1])
+        self.html_block += '''
+            <hr width="50%%">
+            <b>Telescope ToO Information:</b><p>
+            <a href='http://lyra.berkeley.edu/GRB/too/too.php%s'>GRAASP ToO Page</a>
+        ''' % (too_str)
+    
     def add_footer(self):
         update_time = time.ctime(time.time())
         self.html_block += '''
@@ -121,8 +148,9 @@ class GRBHTML:
     
 
 def test():
-    inst = GRBHTML(123456,'/Users/amorgan/Public/TestDir')
-    inst.add_position_info(bat_pos=(123.45,32.52,22),xrt_pos=(123.452,32.622,2.5),reg_path='/Users/amorgan/Desktop/123456.reg')
+    inst = GRBHTML(123416,'/Users/amorgan/Public/TestDir')
+    inst.add_position_info(bat_pos=(13.45,-32.52,22),xrt_pos=(13.452,-32.622,2.5),reg_path='/Users/amorgan/Desktop/123456.reg')
+    inst.add_telescope_info()
     inst.add_finder_chart_info('/Users/amorgan/Desktop/test.png')
     inst.add_footer()
     inst.export_html()
