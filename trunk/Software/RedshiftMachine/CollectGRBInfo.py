a1da03ae43670eb86432d0bd993de80da2b57d1c
amorgan <amorgan@15f48517-4a6d-0410-94cc-8a7d56af1ecd>
2010-02-26 07:28:33 +0000

    Big update!  Added "DeltaErr" option for specifying errors in makeattrarr, test_log_update normalizes, logs, and plots everything you give it, and DistHist plots histograms
    git-svn-id: svn+ssh://lyra.berkeley.edu/Bloom/amorgan/SVNREPO/q_soft@208 15f48517-4a6d-0410-94cc-8a7d56af1ecd

--- trunk/Software/RedshiftMachine/CollectGRBInfo.py
+++ trunk/Software/RedshiftMachine/CollectGRBInfo.py
@@ -369,9 +369,9 @@ class GRBdb:
                 else:
                     self.dict[grb]['z_class'] = 'low_z'
                 
-            if 'RT45' in self.dict[grb] and 'FL' in self.dict[grb]:
-                flosqrtrt45 = self.dict[grb]['FL']*(self.dict[grb]['RT45']**-0.5)
-                self.dict[grb]['FLoverSQRTRT'] = flosqrtrt45  # this should be a measure of S/N
+            if 'T90' in self.dict[grb] and 'FL' in self.dict[grb]:
+                flosqrtrt90 = self.dict[grb]['FL']*(self.dict[grb]['T90']**-0.5)
+                self.dict[grb]['FL_over_SQRT_T90'] = flosqrtrt90  # this should be a measure of S/N
         
             if 'v_mag_isupper' in self.dict[grb]:
                 vmagbinarystr = self.dict[grb]['v_mag_isupper']
@@ -379,6 +379,13 @@ class GRBdb:
                     self.dict[grb]['v_mag_isupper_binary'] = 0
                 else:
                     self.dict[grb]['v_mag_isupper_binary'] = 1
+                    
+            if 'bat_is_rate_trig' in self.dict[grb]:
+                ratebinarystr = self.dict[grb]['bat_is_rate_trig']
+                if ratebinarystr == 'no':
+                    self.dict[grb]['bat_is_rate_trig_binary'] = 0
+                else:
+                    self.dict[grb]['bat_is_rate_trig_binary'] = 1
         
     def log_update_class(self,keylist):
         '''Create offset if there are negative values before taking the logarithm?'''
@@ -406,9 +413,21 @@ class GRBdb:
             nparr[key] = numpy.array(map(lambda x:x[key] if key in x else numpy.nan, self.dict.itervalues()))
         return nparr
         
-    def MakeAttrArr(self,key,poserrkey=None,negerrkey=None):
+    def MakeAttrArr(self,key,poserrkey=None,negerrkey=None,DeltaErr=True):
         '''For the key, create a numpy array of all the values
         Good for getting the mean, std dev, etc.  And for plotting!
+        
+        The errors expected are to be DeltaErr's, that is, distance above and below the mean.
+        i.e value = 5.0, negerr = 0.2, poserr = 0.3
+        Sometimes however they will be reported as High val, low val, medium val.
+        i.e. value = 5.0, negerr = 4.8, poserr = 5.3
+        In this case, set DeltaErr = False
+        
+        Leading to plotting with error bars: 
+        In [428]: db.MakeAttrArr('T90',negerrkey='DT90',poserrkey='DT90')
+        In [429]: db.MakeAttrArr('FL',negerrkey='FL1',poserrkey='FL2',DeltaErr=False)
+        In [430]: pylab.errorbar(y=db.FL['array'],x=db.T90['array'],xerr=[db.T90['negerrarr'],db.T90['poserrarr']],yerr=[db.FL['negerrarr'],db.FL['poserrarr']],fmt='ro')
+        
         
         Dictonary with keywords:
             array - full array for all the entries - use for plotting!
@@ -427,10 +446,16 @@ class GRBdb:
         keydict = {'array':arr,'subarr':subarr,'mean':mean,'median':median,'std':std}
         if poserrkey:
             errarr = numpy.array(map(lambda x:x[poserrkey] if poserrkey in x else numpy.nan, self.dict.itervalues()))
+            if not DeltaErr:
+                errarr = errarr - arr
             keydict['poserrarr'] = errarr
         if negerrkey:
             errarr = numpy.array(map(lambda x:x[negerrkey] if negerrkey in x else numpy.nan, self.dict.itervalues()))
+            if not DeltaErr:
+                errarr = arr - errarr
             keydict['negerrarr'] = errarr
+        # Errors expected are Figure out whether the     
+        
         setattr(self,key,keydict)
    
     def RemOutliers(self,array):
@@ -652,12 +677,46 @@ class GRBdb:
         else:
             grbannotesubplot(self.dict,x_keys=xkeys,y_keys=ykeys,z_keys=zkeys)
 
-    def testplotall(self):
-        keylist = ['EP', 'EP0', 'FL', 'NH_PC', 'NH_WT', 'NH_PC_LATE', 'PK_O_CTS', 'T50', 'T90', 'RT45', 'Z', 'MAX_SNR', 'DT_MAX_SNR']
-        self.plotallvall(keylist=keylist,zval='Z')
-        #['n','n','y','n','n','n','n']
-        pass
+    def test_log_update(self,plot=True,hist=True):
+        self.update_class()
+        keys_to_log = ['xrt_signif', 'bat_rate_signif', 'bat_image_signif', 'EP', 'EP0', 'FL', 'NH_PC', 'NH_WT', 'NH_PC_LATE', 'PK_O_CTS', 'T90', 'RT45', 'MAX_SNR', 'DT_MAX_SNR','peakflux','bat_inten','xrt_column','FL_over_SQRT_T90']
+        self.log_update_class(keys_to_log)
+        keys_to_norm = ['log_xrt_signif', 'log_bat_rate_signif', 'log_bat_image_signif','log_EP', 'log_EP0', 'log_FL', 'log_NH_PC', 'log_NH_WT', 'log_NH_PC_LATE', 'log_PK_O_CTS', 'log_T90', 'log_RT45', 'log_MAX_SNR', 'log_DT_MAX_SNR', 'log_peakflux', 'log_bat_inten', 'log_xrt_column','log_FL_over_SQRT_T90']
+        self.norm_update_class(keys_to_norm)
+        keys_to_plot = ['Z', 'v_mag_isupper_binary', 'bat_is_rate_trig_binary', 'norm_log_xrt_signif', 'norm_log_bat_rate_signif', 'norm_log_bat_image_signif', 'norm_log_EP', 'norm_log_EP0', 'norm_log_FL', 'norm_log_NH_PC', 'norm_log_NH_WT', 'norm_log_NH_PC_LATE', 'norm_log_PK_O_CTS', 'norm_log_T90', 'norm_log_RT45', 'norm_log_MAX_SNR', 'norm_log_DT_MAX_SNR', 'norm_log_peakflux', 'norm_log_bat_inten', 'norm_log_xrt_column','norm_log_FL_over_SQRT_T90']
+        keys_to_hist = [ 'norm_log_xrt_signif', 'norm_log_bat_rate_signif', 'norm_log_bat_image_signif', 'norm_log_EP', 'norm_log_EP0', 'norm_log_FL', 'norm_log_NH_PC', 'norm_log_NH_WT', 'norm_log_NH_PC_LATE', 'norm_log_PK_O_CTS', 'norm_log_T90', 'norm_log_RT45', 'norm_log_MAX_SNR', 'norm_log_DT_MAX_SNR', 'norm_log_peakflux', 'norm_log_bat_inten', 'norm_log_xrt_column','norm_log_FL_over_SQRT_T90']
+        if plot:
+            self.plotallvall(keylist=keys_to_plot,zval='Z')
+        if hist:
+            self.DistHist(keylist=keys_to_hist)
+        
+    def DistHist(self,keylist):
+        self.MakeAttrArr('T90',negerrkey='DT90',poserrkey='DT90')
+        long_ind=numpy.nonzero(self.T90['array'] > 2.0) #indeces of long bursts
+        remove_short = True
+        for key in keylist:
+            attr = getattr(self,key)
+            if remove_short:
+#                pylab.hist(attr['subarr'],bins=15)  # With Short Bursts
+                testarr = attr['array'][long_ind] 
+                pylab.hist(testarr[numpy.nonzero(numpy.nan_to_num(testarr))] ,bins=15)  # Without Short Bursts
+            else:
+                pylab.hist(attr['array'],bins=15)
+            pylab.xlabel(key)
+
+            figname = self.name
+            figname += '_hist_'+key
+            figname += '.png'
+            figoutdir = storepath + 'figures/'+figname
+            pylab.savefig(figoutdir)
+            pylab.close()
     
+    # def testplotall(self):
+    #     keylist = ['EP', 'EP0', 'FL', 'NH_PC', 'NH_WT', 'NH_PC_LATE', 'PK_O_CTS', 'T50', 'T90', 'RT45', 'Z', 'MAX_SNR', 'DT_MAX_SNR']
+    #     self.plotallvall(keylist=keylist,zval='Z')
+    #     #['n','n','y','n','n','n','n']
+    #     pass
+    # 
     def printall(self,keywordlist,suppress = True):
         '''Print all the keywords in the keyword list in the collected dictionary
         If suppress, then only print them out if all keywords are present for a 
