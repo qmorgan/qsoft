e58ba9a660237e2d74222bb03b1eca7428358f06
Adam N. Morgan <qmorgan@gmail.com>
2010-12-30 18:26:58 -0800

    Improvement of the jittering allowed within ColorScatter - now the user can save the jittering used for a particular array for later overplotting of symbols around the original plot.

--- trunk/Software/Plotting/ColorScatter.py
+++ trunk/Software/Plotting/ColorScatter.py
@@ -1,5 +1,6 @@
 import pylab
 import scipy
+import numpy
 import matplotlib
 from numpy import *
 from scipy import *
@@ -9,7 +10,7 @@ import scipy.interpolate as interpolate
 def Bumps(x,y,z=None):
     pdict =  {'a':[3,2,1],'b':[2,3,2],'c':[1,1,3]}
     x = arange(len(pdict.values()[0]))
-    for element in plist
+    for element in plist:
        pylab.plot(x,element)
     
 
@@ -21,19 +22,51 @@ def ColorScatterExample():
 
 
 def ColorScatter(x,y,z=None,cmap='jet',colorbar=True,discrete=0,yjitter=0.0,\
-    xjitter=0.0,marker='o'):
+    xjitter=0.0,marker='o',retjitter=False, **kwargs):
     '''set discrete to N for splitting up into N values
     yjitter sets a percent random jitter in the y direction to help distinguish
     overlapping values.  
-    '''
-    #Convert to arrays
     
+    If retjitter=True, return a tuple of the jitter arrays.
+    
+    The jitter parameters can be either a number, to specify a randomly generated
+    jitter (multiplied by the  value of the jitter parameter), or it can be an
+    array of the length of the axis of pre-defined values to bump the array by.
+    This is useful if you want to overplot the same jitter values twice.  
+    
+    E.g., if you have some special values that you want to overplot a ring around,
+    separate the array into special and non-special values.  Plot the non-special
+    values first as a normal scatter plot, then the special values as a normal 
+    scatter plot (but save the jitter values with retjitter), and then overplot a 
+    ring around the same values.
+    
+    '''
+        
     if yjitter:
-        y_lim_len = pylab.ylim()[1] - pylab.ylim()[0]
-        y = y + random(len(y))*yjitter*y_lim_len
+        try:
+            list(yjitter) # check if a list or array
+            yjitter = numpy.array(yjitter)
+            assert(len(yjitter)==len(y))
+            y = y + yjitter
+        except:
+            y_lim_len = pylab.ylim()[1] - pylab.ylim()[0]
+            yjitter = (pylab.random(len(y))-0.5)*yjitter*y_lim_len
+            y = y + yjitter
+    else:
+        yjitter = pylab.zeros(len(y))
+        
     if xjitter:
-        x_lim_len = pylab.xlim()[1] - pylab.xlim()[0]
-        x = x + random(len(x))*xjitter*x_lim_len
+        try:
+            list(xjitter) # check if a list or array
+            xjitter = numpy.array(xjitter)
+            assert(len(xjitter)==len(x))
+            x = x + xjitter
+        except:
+            x_lim_len = pylab.xlim()[1] - pylab.xlim()[0]
+            xjitter = (pylab.random(len(x))-0.5)*xjitter*x_lim_len
+            x = x + xjitter
+    else:
+        xjitter = pylab.zeros(len(x))        
     
     if z != None:
         z = array(z)
@@ -43,10 +76,13 @@ def ColorScatter(x,y,z=None,cmap='jet',colorbar=True,discrete=0,yjitter=0.0,\
             except:
                 N = 2
             cmap = cmap_discretize(pylab.cm.cool_r,N)
-        sc = pylab.scatter(x,y,c=z,cmap=cmap,marker=marker)
+        sc = pylab.scatter(x,y,c=z,cmap=cmap,marker=marker,**kwargs)
         if colorbar: pylab.colorbar(sc)
     else:
-        sc = pylab.scatter(x,y,marker=marker)
+        sc = pylab.scatter(x,y,marker=marker,**kwargs)
+        
+    if retjitter:
+        return((list(xjitter),list(yjitter)))
 
 def cmap_discretize(cmap, N):
     """Return a discrete colormap from the continuous colormap cmap.
