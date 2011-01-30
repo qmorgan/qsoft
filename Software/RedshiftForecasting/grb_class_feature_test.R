4bb8e7c7915bb6ca56ce75ee56cde31e22cba3f2
james <james@james-laptop.(none)>
2011-01-29 17:34:10 -0800

    added performance pdf plots

--- Software/RedshiftForecasting/grb_class_feature_test.R
+++ Software/RedshiftForecasting/grb_class_feature_test.R
@@ -1,68 +1,51 @@
 ## tests how performance changes as more useless features are added to data
 ##
 ## by James Long
-## date Dec 22, 2010
+## created Dec 22, 2010, updated Jan 28, 2011
 ##
 
-library('foreign')
+# remove everything from R's memory
+rm(list=ls(all=TRUE))
 
+library('foreign')
 source('grb_class.R')
 source('algorithm1/algorithm1.R')
 
-filename='algorithm1/uvot_no_error.arff'
-data1 = read.arff(filename)
-Z = data1$Z
-data1 = cleanData(data1,4) # define above 4 as high, below 4 as low
-data1 = subset(data1,select=((names(data1) %in% c("class","uvot_detection"))))
 
+# parameters to random forest function
+weight = 10^1 # how much are high bursts weighted
+alpha = .3 # fraction to follow up on
 
-## get some numbers about the data
-print('number of features being used:')
-print(length(data1) - 1)
-print('number of observations:')
-print(nrow(data1))
-print('number of high grbs:')
-print(sum(data1$class=="high"))
-print('number of low grbs:')
-print(sum(data1$class=="low"))
-print('names of features:')
-print(names(data1))
-table(data1$class,data1$uvot_detection)
+# numbers of useless features to try
+useless = 1:50
 
-
-implement(data1,Z)
-
-
-####
-#### have to define inputs for forest_run
-####
-weight = 10^5
-classes = data1$class
-high_cutoff = 4
-num_high = sum(classes == "high")
-num_low = sum(classes == "low")
-useless_features = matrix(rnorm(length(Z)*10),ncol=10)
-features = data.frame(data1$uvot_detection,useless_features)
-
-
-
-
-useless = 10:70
+data_obj = read_data()
+features = data_obj$features
 result = rep(0,length(useless))
+
+### so the good features we are using are, could try
+### replacing these with just uvot
+names(data_obj$features)
+
+### at each iteration we add 1 more useless feature
+### and output high as high / (high as high + high as low)
+### to the results vector
 for(i in 1:length(result)){
   print(paste(i,"of",length(result),"runs"))
-  useless_features = matrix(rnorm(length(Z)*useless[i]),ncol=useless[i])
-  features = data.frame(data1$uvot_detection,useless_features)
-  output = forest_run()
+  useless_features = matrix(rnorm(length(data_obj$Z)*useless[i]),ncol=useless[i])
+  data_obj$features = data.frame(features,useless_features)
+  output = forest_run(data_obj=data_obj,weight=weight,alpha=alpha)
   result[i] = output$high_as_high / (output$high_as_high + output$high_as_low)
 }
 
 
-
-pdf('performance_vs_useless_features_log.pdf')
+### plot the results to see how performance degrades
+plot_title = paste('performance_vs_useless_features_weight_',weight,'.pdf',sep="")
+pdf(plot_title)
 linear.fit = lm(result ~ useless)
-plot(useless,result,ylim=c(0,max(max(result),13/15)),xlab="Number of Useless Features",ylab="Fraction of Highs Classified as High",main="RF Performance vs. Number of Useless Features: Weight 7 on High")
-abline(h=13/17,col='grey')
+main_title = paste("RF Performance vs. Number of Useless Features: Weight",weight,"on High")
+plot(useless,result,ylim=c(0,max(max(result),13/15)),xlab="Number of Useless Features",ylab="Fraction of Highs Classified as High",main=main_title)
+abline(h=13/17,col='grey') # if we follow up on all uvot no, alpha > 49 / 151 in order to do this
 abline(linear.fit,col='grey')
 dev.off()
 
