489b6c34a6237de9ac39b13c8da5b0a930f2999a
Joseph Richards <joeyrichar@gmail.com>
2011-02-02 11:31:31 -0800

    updated grb_prediction_test_2.R

--- Software/RedshiftForecasting/grb_prediction_test_2.R
+++ Software/RedshiftForecasting/grb_prediction_test_2.R
@@ -10,7 +10,16 @@ weight = 10^1 # how much are high bursts weighted
 wts = ifelse(classes=="high",weight,1)
 rf = forest.fit(features,classes,mtry=NULL,weights=wts,n.trees=500)
 newdata = read_data(filename='./algorithm1/testdata.arff')
-nbursts = dim(newdata$features)[1]
-allfeatures = rbind(newdata$features,features)
-newfeatures = data.frame(allfeatures[1:nbursts,])
+newfeatures = newdata$features
+nbursts = dim(newfeatures)[1]
+# this is the fix: have to loop thru the columns and ensure that the
+# factors have the same levels as the original data...ugh!
+for(jj in 1:dim(newfeatures)[2]){
+  if(is.factor(newfeatures[,jj])){
+    cat("Feature",names(newfeatures)[jj],"is factor\n")
+    newfeatures[,jj] = factor(newfeatures[,jj],levels=levels(features[,jj]))
+  }
+}
+
 pred.val = forest.pred(rf,newfeatures)
+
