60a92a02ae1d333a07ed078892b7b5379ee6b0e0
Adam N. Morgan <qmorgan@gmail.com>
2011-02-03 22:08:39 -0800

    Added wrapper function to make representative plots for a given dataset

--- Software/RedshiftForecasting/grb_class.R
+++ Software/RedshiftForecasting/grb_class.R
@@ -428,3 +428,19 @@ forest.pred = function(forest,xnew){
   return(list(alpha.hat = alpha.hat,prob.high=predictions[,2],prob.low=predictions[,1]))
 }
 
+# Wrapper to make all representative plots for a given dataset
+make_forest_plots = function(data_string="reduced"){
+   data_filename = paste("./Data/GRB_short+outliers+noZ_removed_",data_string,".arff",sep="")
+   data_results_dir = paste("smooth_weights_",data_string,sep="")
+   obj_func_name = paste("objective_fcn_",data_string,".pdf",sep="")
+   bumps_plot_name = paste("forest_order_bumps_",data_string,".pdf",sep="")
+   bumps_text_name = paste("forest_order_bumps_",data_string,".txt",sep="")
+
+   mydata = read_data(filename=data_filename,high_cutoff=4)
+   smooth_random_forest_weights(data_obj = mydata,results_dir=data_results_dir)
+   fres = extract_stats(data_obj = mydata, forest_res_dir=data_results_dir)
+   fres_ordered = order_residuals(fres)
+   make_obj_fcn_plot(fres_ordered, data_obj = mydata, imagefile=obj_func_name)
+   fres_ordered = order_residuals(fres,reverse=TRUE)
+   make_bumps_plot(fres_ordered, data_obj = mydata, imagefile=bumps_plot_name,textfile=bumps_text_name)
+}
