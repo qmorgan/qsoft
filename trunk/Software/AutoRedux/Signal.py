76ab12586d71fb32de70f45364289e42e30f3051
Adam N. Morgan <qmorgan@gmail.com>
2011-02-08 14:01:02 -0800

    fixed typo

--- trunk/Software/AutoRedux/Signal.py
+++ trunk/Software/AutoRedux/Signal.py
@@ -174,7 +174,7 @@ def _mail_html(gcn,mail_to,clobber=False,tweet=True):
                 try:
                     # python-twitter requires some kind of oAuth authentication now which is a pain
                     # so just use tweetymail, linked to the q.mailbot account.
-                    tweetymail = tweet@tweetymail.com
+                    tweetymail = 'tweet@tweetymail.com'
                     # import twitter # requires http://code.google.com/p/python-twitter/
                     import tinyurl # requires http://pypi.python.org/pypi/TinyUrl/0.1.0 
                     bigurl = 'http://astro.berkeley.edu/~amorgan/Swift/%i/' % int(gcn.triggerid)
