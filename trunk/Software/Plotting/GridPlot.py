05147f307fd7970d2823dca978998de62bf32e7f
Adam N. Morgan <qmorgan@gmail.com>
2011-02-27 23:41:31 -0800

    Several changes; made gridplot a callable function, made more generalized, added in point size determination, changed to scatter plot, added option to not include tick labels.

--- trunk/Software/Plotting/GridPlot.py
+++ trunk/Software/Plotting/GridPlot.py
@@ -3,84 +3,78 @@
 import math
 from pylab import figure, show, setp
 from numpy import sin, cos, exp, pi, arange
+from Plotting.ColorScatter import ColorScatter as scatter
 import numpy
 
 # Draw a custom axis for the colorbar: 
 # plt.colorbar(sc,cax=plt.gcf().add_axes((0.90,0.1,0.02,0.8)))
 
 
-N = 3
+def GridPlot(data,labels=None,no_tick_labels=False):
+    
+    ## EDITABLE
+    right_buffer = 0.1
+    bottom_buffer = 0.1
+    left_buffer = 0.1
+    top_buffer = 0.1
+    ## end editable
+    
+    matr = numpy.array(data)
+    
+    N = len(matr)
+    if labels and len(labels) != N:
+        print '''
+        Length of labels array (%i) != length of data array (%i);
+        not including labels.''' % (len(labels),N)
+        labels = None
 
-A = numpy.array([1,2,3,4])
-B = numpy.array([2,7,3,6])
-C = numpy.array([6,3,4,1])
+    fig = figure()
 
-matr = numpy.array([A,B,C])
-labels = ['A','B','C']
+    point_size = numpy.ceil(500/(len(matr[0])*N))
 
-fig = figure()
-t = arange(0.0, 2.0, 0.01)
+    horiz_width = 1.0 - right_buffer - left_buffer
+    vert_width = 1.0 - top_buffer - bottom_buffer
 
-## EDITABLE
-right_buffer = 0.1
-bottom_buffer = 0.1
-## end editable
+    hw = horiz_width/N # subplot width
+    vw = vert_width/N 
 
-left_buffer = 0.1
-top_buffer = 0.1
-horiz_width = 1.0 - right_buffer - left_buffer
-vert_width = 1.0 - top_buffer - bottom_buffer
-
-hw = horiz_width/N # subplot width
-vw = vert_width/N 
-
-count = 0
-for ii in arange(N):
-    for jj in arange(N):
-        qq = 0.1 + ii*hw
-        rr = (0.9 - vw) - jj*vw
-        ax1 = fig.add_axes([qq,rr,hw,vw])
-        ax1.plot(matr[ii],matr[jj])
+    count = 0
+    for ii in arange(N):
+        for jj in arange(N):
+            qq = left_buffer + ii*hw
+            rr = ((1-top_buffer) - vw) - jj*vw
+            ax1 = fig.add_axes([qq,rr,hw,vw])
+            #ax1.scatter(matr[ii],matr[jj])
+            scatter(matr[ii],matr[jj],s=point_size)
         
-        if jj == N-1:
-            ax1.set_xlabel(labels[ii])
+            if jj == N-1 and labels:
+                ax1.set_xlabel(labels[ii])
         
-        if ii == 0:
-            ax1.set_ylabel(labels[jj])
+            if ii == 0 and labels:
+                ax1.set_ylabel(labels[jj])
             
-        if ii != 0:
-            setp(ax1.get_yticklabels(), visible=False)
+            if no_tick_labels and ii == 0:
+                setp(ax1.get_yticklabels(), visible=False)
+            if ii != 0:
+                setp(ax1.get_yticklabels(), visible=False)
                 
-        # turn off x ticklabels for all but the lower axes
-        if jj != N-1:
-            setp(ax1.get_xticklabels(), visible=False)
+            # turn off x ticklabels for all but the lower axes
+            if no_tick_labels and jj == N-1:
+                setp(ax1.get_xticklabels(), visible=False)
+            if jj != N-1:
+                setp(ax1.get_xticklabels(), visible=False)
         
             
 
-# ax1 =fig.add_axes([0.1, 0.7, 0.8, 0.2], **axprops)
-# ax1.plot(t, s1)
-# ax1.set_ylabel('S1', **yprops)
-# 
-# axprops['sharex'] = ax1
-# axprops['sharey'] = ax1
-# # force x axes to remain in register, even with toolbar navigation
-# ax2 = fig.add_axes([0.1, 0.5, 0.3, 0.2], **axprops)
-# 
-# 
-# 
-# ax2.plot(t, s2)
-# ax2.set_ylabel('S2', **yprops)
-# 
-# ax3 = fig.add_axes([0.1, 0.3, 0.8, 0.2], **axprops)
-# ax3.plot(t, s4)
-# ax3.set_ylabel('S3', **yprops)
-# 
-# ax4 = fig.add_axes([0.1, 0.1, 0.8, 0.2], **axprops)
-# ax4.plot(t, s4)
-# ax4.set_ylabel('S4', **yprops)
-# 
-# # turn off x ticklabels for all but the lower axes
-# for ax in ax1, ax2, ax3:
-#     setp(ax.get_xticklabels(), visible=False)
+    show()
 
-show()
+def TestPlot():
+    A = numpy.array([1,2,3,4])
+    B = numpy.array([2,7,3,6])
+    C = numpy.array([6,3,4,1])
+    D = numpy.array([5,2,4,5])
+
+    data = numpy.array([A,B,C,D])
+    labels = ['A','3','C','D']
+
+    GridPlot(data,labels=labels, no_tick_labels=True) 
