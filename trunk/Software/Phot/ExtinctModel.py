c50fc17c1d84e0d65fb74a8c636dfebbbfaa06d9
Adam N. Morgan <qmorgan@gmail.com>
2012-02-15 16:27:48 -0800

    Extended the test case to make several diagnostic plots to see how extinction varies as certain parameters change.

--- trunk/Software/Phot/ExtinctModel.py
+++ trunk/Software/Phot/ExtinctModel.py
@@ -24,7 +24,7 @@ class Extinction:
         self.funred = self.flux * 10.**(0.4*self.curve)
     
     
-    def plotModel(self,fig=None,color='blue',show=True):    
+    def plotModel(self,fig=None,color='blue',show=True,loglog=False):    
         if not hasattr(self,'curve'):
             raise Exception("Need to run EvalCurve first")
         if not hasattr(self,'funred'):
@@ -41,6 +41,8 @@ class Extinction:
         ax.plot(self.wave,self.flux,linewidth=1,color=color,linestyle='dashed')
         ax.set_xlabel('Wavelength')
         ax.set_ylabel('Flux Density')
+        if loglog:
+            ax.loglog()
         if show:
             plt.show()
         else:
@@ -169,9 +171,12 @@ class FM(Extinction):
         
         
         
-def avgsmc():
-    '''In the average SMC fit, the only free parameters are Av and Beta'''
-    R_V = 2.74
+def avgsmc(R_V = 2.74):
+    '''
+    These models should just take Av, Beta, flux vector, and wavelength vector, 
+    and return a new flux vector..
+    
+    In the average SMC fit, the only free parameters are Av and Beta'''    
     c1 = -4.959
     c2 =  2.264
     c3 = 0.389
@@ -181,33 +186,170 @@ def avgsmc():
     fmmodel=FM(R_V,c1,c2,c3,c4,gamma,x0)
     return fmmodel
     
-def avglmc():
-    R_V=3.1
-    c1=-2.16
-    c2=1.31
-    c3=1.92
-    c4=0.42
-    gamma=1.05
-    x0=4.626
+def lmc2(R_V = 3.1):   
+    c1 = -2.16
+    c2 = 1.31
+    c3 = 1.92
+    c4 = 0.42
+    gamma = 1.05
+    x0 = 4.626
+    fmmodel=FM(R_V,c1,c2,c3,c4,gamma,x0)
+    return fmmodel
+
+def avglmc(R_V = 3.2):    
+    c1 = -1.28
+    c2 = 1.11
+    c3 = 2.73
+    c4 = 0.64
+    gamma = 0.91
+    x0 = 4.596  
+    fmmodel=FM(R_V,c1,c2,c3,c4,gamma,x0)
+    return fmmodel
+    
+def avgmw(R_V = 3.1,c3 = 3.23,c4 = 0.41):
+    '''Take a look at Reichart 2001 for an empirical relation between
+    c1, c2, and R_V to remove the degeneracy between them, recommended if you
+    dont have amazing enough data to try to constrain all 6 parameters, but 
+    amazing enough to try to constrain c3 and c4.
+    
+    Gamma and x0 can generally be fixed if you do not have enough free paraamters 
+    to break the degeneracy.
+    '''
+    c2 = -0.824 + 4.717/R_V
+    c1 = 2.030 - 3.007*c2
+    gamma = 0.99
+    x0 = 4.596
     fmmodel=FM(R_V,c1,c2,c3,c4,gamma,x0)
     return fmmodel
         
 def Test120119A():
     z=1.72
-    w=1250. + np.arange(80)*50.
+    wv_angst=np.array([  6470. ,   7865.,  12500. ,  16500. ,  21500. ])
+    wv_angst_rest = wv_angst/(1+z)
+    
+    w=1250. + np.arange(100)*50.
     f = w*0. + 1
-    a=avglmc()
+    a=lmc2(R_V=3.1)
     a.EvalCurve(w,-0.1)
     a.UnreddenFlux(f)
-    fig = a.plotModel(show=False,color='red')
-    b=avgsmc()
+    fig = a.plotModel(show=False,color='green')
+    b=avgsmc(R_V=3.1)
     b.EvalCurve(w,-0.1)
     b.UnreddenFlux(f)
     fig=b.plotModel(fig=fig,show=False,color='blue')
+    c=avgmw(R_V=3.1)
+    c.EvalCurve(w,-0.1)
+    c.UnreddenFlux(f)
+    fig=c.plotModel(fig=fig,show=False,color='red')
+    
+    fig.text(0.7,0.5,'E(B-V)=-0.1',color='black')
+    fig.text(0.7,0.46,'Rv=3.1',color='black')
+    fig.text(0.7,0.42,'SMC',color='blue')
+    fig.text(0.7,0.38,'LMC',color='green')
+    fig.text(0.7,0.34,'MW',color='red')
+    fig.show()
+    # Note in the figure above, all extinction models converge in the optical
+    # when R_V is set to a given number.
     
     
-    wv_angst=np.array([  6470. ,   7865.,  12500. ,  16500. ,  21500. ])
-    wv_angst_rest = wv_angst/(1+z)
+    # Now see how a change in Rv manifests with the other parameters set as SMC
+    av=-0.31
+    rv=4.3
+    q=avgsmc(R_V=rv)
+    q.EvalCurve(w,av/rv)
+    q.UnreddenFlux(f)
+        
+    rv=3.9
+    a=avgsmc(R_V=rv)
+    a.EvalCurve(w,av/rv)
+    a.UnreddenFlux(f)
+    
+    rv=3.5
+    b=avgsmc(R_V=rv)
+    b.EvalCurve(w,av/rv)
+    b.UnreddenFlux(f)
+    
+    rv=3.1
+    c=avgsmc(R_V=rv)
+    c.EvalCurve(w,av/rv)
+    c.UnreddenFlux(f)
+    
+    rv=2.7
+    d=avgsmc(R_V=rv)
+    d.EvalCurve(w,av/rv)
+    d.UnreddenFlux(f)
+    
+    rv=2.4
+    e=avgsmc(R_V=rv)
+    e.EvalCurve(w,av/rv)
+    e.UnreddenFlux(f)
+    
+    fig2=q.plotModel(show=False,color='purple')
+    fig2=a.plotModel(fig=fig2,show=False,color='blue')
+    fig2=b.plotModel(fig=fig2,show=False,color='green')
+    fig2=c.plotModel(fig=fig2,show=False,color='yellow')
+    fig2=d.plotModel(fig=fig2,show=False,color='orange')
+    fig2=e.plotModel(fig=fig2,show=False,color='red')
+    fig2.text(0.7,0.54,'SMC law',color='black')
+    fig2.text(0.7,0.5,'Av=-0.31',color='black')
+    fig2.text(0.7,0.46,'Rv=4.3',color='purple')
+    fig2.text(0.7,0.42,'Rv=3.9',color='blue')
+    fig2.text(0.7,0.38,'Rv=3.5',color='green')
+    fig2.text(0.7,0.34,'Rv=3.1',color='yellow')
+    fig2.text(0.7,0.30,'Rv=2.7',color='orange')
+    fig2.text(0.7,0.26,'Rv=2.4',color='red')
+    fig2.show()
+    
+    #now see how varying Av for a set Rv will affect things.
+    
+    rv=2.74 #average smc
+    av=-0.3
+    q=avgsmc(R_V=rv)
+    q.EvalCurve(w,av/rv)
+    q.UnreddenFlux(f)
+    
+    av=-0.25
+    a=avgsmc(R_V=rv)
+    a.EvalCurve(w,av/rv)
+    a.UnreddenFlux(f)
+    
+    av=-0.20
+    b=avgsmc(R_V=rv)
+    b.EvalCurve(w,av/rv)
+    b.UnreddenFlux(f)
+    
+    av=-0.15
+    c=avgsmc(R_V=rv)
+    c.EvalCurve(w,av/rv)
+    c.UnreddenFlux(f)
+    
+    av=-0.10
+    d=avgsmc(R_V=rv)
+    d.EvalCurve(w,av/rv)
+    d.UnreddenFlux(f)
+    
+    av=-0.05
+    e=avgsmc(R_V=rv)
+    e.EvalCurve(w,av/rv)
+    e.UnreddenFlux(f)
+    
+    fig3=q.plotModel(show=False,color='purple',loglog=False)
+    fig3=a.plotModel(fig=fig3,show=False,color='blue')
+    fig3=b.plotModel(fig=fig3,show=False,color='green')
+    fig3=c.plotModel(fig=fig3,show=False,color='yellow')
+    fig3=d.plotModel(fig=fig3,show=False,color='orange')
+    fig3=e.plotModel(fig=fig3,show=False,color='red')
+    fig3.text(0.7,0.54,'SMC law',color='black')
+    fig3.text(0.7,0.5,'Rv=2.74',color='black')
+    fig3.text(0.7,0.46,'Av=-3.0',color='purple')
+    fig3.text(0.7,0.42,'Av=-2.5',color='blue')
+    fig3.text(0.7,0.38,'Av=-2.0',color='green')
+    fig3.text(0.7,0.34,'Av=-1.5',color='yellow')
+    fig3.text(0.7,0.30,'Av=-1.0',color='orange')
+    fig3.text(0.7,0.26,'Av=-0.5',color='red')
+    
+    fig3.show()
+    
     
     return b
     
