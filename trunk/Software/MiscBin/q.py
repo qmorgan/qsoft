46c59ec10450a9cbb2d6aa479d5598af4adcef18
Adam N. Morgan <qmorgan@gmail.com>
2012-03-07 16:08:46 -0800

    Changed the mag2flux function to have the option of specifying a qObs.filt object to do the flux conversion.

--- trunk/Software/MiscBin/q.py
+++ trunk/Software/MiscBin/q.py
@@ -52,14 +52,31 @@ def mag2abmag(mag, zpflux):
     AB_mag = -2.5*numpy.log10(q.mag2flux(mag,0,zpflux)) - 48.60
     return AB_mag
 
-def mag2flux(mag_1=99,mag_2=99,flux_2=0):
-    """Given a magnitude and a flux """
+def mag2flux(mag_1=99,mag_2=99,flux_2=0,filt=None):
+    """Given an input magnitude and a second magnitude + corresponding flux 
+    OR 
+    Given an input magnitude and an instance of the filt object,
+    convert the input magnitude to a flux (in microjanskies if filt object is 
+    specified)
+    
+    The filt object (in qObs.py) contains the required zeropoint flux in uJy
+    """
     if mag_1 == 99:
-        mag_1 = raw_input('Please enter mag_1: ')
-    if mag_2 == 99:
-        mag_1 = raw_input('Please enter mag_2: ')
-    if flux_2 == 0:
-        flux_2 = raw_input('Please enter flux_2: ')
+        mag_1 = float(raw_input('Please enter mag_1: '))
+    if filt:
+        if mag_2 != 99 or flux_2 !=0:
+            raise ValueError('Cannot specify both filt and secondary mag/flux')
+        else:
+            if str(filt.__class__).split('.')[-1] != 'filt':
+                raise ValueError('Filt is not an instance of the filt object')
+            mag_2 = 0
+            flux_2 = filt.zpflux
+    else:                
+        if mag_2 == 99:
+            mag_1 = raw_input('Please enter mag_2: ')
+        if flux_2 == 0:
+            flux_2 = raw_input('Please enter flux_2: ')
+    
     flux_1 = flux_2 * 10**(-0.4*(mag_1 - mag_2))
     return flux_1
 
