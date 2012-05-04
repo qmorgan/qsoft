adb15dc3b73d04c8e21e50e77b094cfedcf72794
Adam N. Morgan <qmorgan@gmail.com>
2012-05-03 17:40:28 -0700

    Added plotting option to qspline

--- trunk/Software/Modelling/qSpline.py
+++ trunk/Software/Modelling/qSpline.py
@@ -1,6 +1,8 @@
 import numpy as np
 import os
 import sys
+from matplotlib import rc
+import matplotlib.pyplot as plt
 
 if not os.environ.has_key("Q_DIR"):
     print "You need to set the environment variable Q_DIR to point to the"
@@ -10,7 +12,7 @@ splinedir = os.environ.get("Q_DIR") + '/trunk/Software/Modelling/'
 storepath = os.environ.get("Q_DIR") + '/store/'
 loadpath = os.environ.get("Q_DIR") + '/load/'
 
-def qSpline(xvals,yvals,yerrvals,xgrid,allow_out_of_bounds=False):
+def qSpline(xvals,yvals,yerrvals,xgrid,allow_out_of_bounds=False,plot=False):
     
     if not allow_out_of_bounds:
         if min(xgrid) < min(xvals):
@@ -18,7 +20,30 @@ def qSpline(xvals,yvals,yerrvals,xgrid,allow_out_of_bounds=False):
         if max(xgrid) > max(xvals):
             raise ValueError('Desired xgrid out of bounds - max value too high')
         
+    newyarr, spline_model_errarr = _callRegrSpline(xvals,yvals,yerrvals,xgrid)
     
+    if plot:
+        rc('font', family='Times New Roman')
+        fig=plt.figure()
+        ax=fig.add_axes([0.1,0.1,0.8,0.8])
+        
+
+        print "Rerunning spline fit for plot"
+        model_xgrid = np.linspace(xvals.min(),xvals.max(),num=500)
+        modelyarr, modelyerrarr = _callRegrSpline(xvals,yvals,yerrvals,model_xgrid)
+        
+
+        # plot model
+        ax.plot(model_xgrid,modelyarr,lw=2,color='black')
+        ax.plot(model_xgrid,modelyarr-modelyerrarr,lw=1,color='grey')
+        ax.plot(model_xgrid,modelyarr+modelyerrarr,lw=1,color='grey')
+        # plot data
+        ax.errorbar(xvals,yvals,yerr=yerrvals,color='black',fmt='o')
+        
+        fig.show()    
+    return newyarr, spline_model_errarr
+    
+def _callRegrSpline(xvals,yvals,yerrvals,xgrid):
     fitpath = storepath + 'regrSpline_fit.txt'
     fiterrpath = storepath + 'regrSpline_fiterr.txt'
     write_ypath = storepath + 'regrSpline_y_in.txt'
