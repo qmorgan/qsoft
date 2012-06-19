f29c36921d650bc6ae5f27537ef8f1c2f7a7de2f
Adam N. Morgan <qmorgan@gmail.com>
2012-06-19 10:28:23 -0700

    Fixed brokenpowerlaw function to work with both arrays and single-values

--- trunk/Software/Modelling/Functions.py
+++ trunk/Software/Modelling/Functions.py
@@ -4,15 +4,21 @@ def DecayingExponential(timearr,Av_0,Av_1,tau):
     Av = Av_0 + Av_1*np.exp(-1*timearr/tau)
     return Av
 
-def BrokenPowerLaw(timearr,Av_0,Av_1,Av_2):
+def BrokenPowerLaw(time,Av_0,Av_1,Av_2):
     # Av_2 = tbreak
-    Avlist=[]
-    for time in timearr:
+    try: #try if its an array
+        Avlist=[] 
+        for tim in time:
+            if tim <= Av_2:
+                Av = Av_0/(Av_2**Av_1)*tim**Av_1
+            if tim > Av_2:
+                Av = Av_0 
+            Avlist.append(Av)
+        Av = Avlist
+    except:
         if time <= Av_2:
             Av = Av_0/(Av_2**Av_1)*time**Av_1
         if time > Av_2:
             Av = Av_0 
-        Avlist.append(Av)
-    Avarr = np.array(Avlist)    
-    return Avlist
+    return Av
     ###
