22e158f7243b9fd1deb6fb633d60dc3050f54cad
Adam N. Morgan <qmorgan@gmail.com>
2013-08-07 23:33:40 -0700

    Advanced the table infrastructure; created detection diagnostic images

--- trunk/Software/Cell/Migration.py
+++ trunk/Software/Cell/Migration.py
@@ -89,11 +89,81 @@ def tiff2fits(filelist,truncate_filename=True):
             os.system(magickcommand)
             outlist += outpath
         except:
-            print "Do not have Imagemagick, cannot convert svg to png"
+            print "Do not have Imagemagick, cannot convert tif to fits"
             
     return outlist
 
 
+def _convert_verification_pngs():
+    '''This takes several minutes, recommended only if you need to verify that
+    your detections are being populated correctly 
+    '''
+    ap_list = glob.glob('ap_0*.fits')
+    for ap_img in ap_list:
+        png_name = ap_img.replace('fits','png')
+        im_cmd = 'convert %s -fx "log(abs((u.r+u.g+u.b)*80))" %s' % (ap_img,png_name)
+        os.system(im_cmd)
+        # cp_cmd = 'cp %s %s' % (png_name,png_name.replace('ap_','detections_'))
+        # os.system(cp_cmd)
+def _overlay_verification_text(obj_str,xx,yy,theta,double_size=True):    
+    
+    if theta <= 0: 
+        ang = abs(theta)
+    else: 
+        ang = 360. - theta
+    
+    if not double_size:
+        xpos = xx - 256.  #subtracting 256 since changing gravity setting not only 
+        # refers to the position on the background image, but also the position 
+        # in the overlay image that is to be drawn.
+        ypos = 512.- yy -256.
+    else:
+        xpos = xx*2 - 512.
+        ypos = 1024. - yy*2 - 512
+    
+    if xpos < 0:
+        xsign="-"
+    else:
+        xsign="+"
+    if ypos < 0:
+        ysign="-"
+    else:
+        ysign="+"
+    
+    im_cmd_append = ' -fill red -annotate %fx%fXSIGN%fYSIGN%f "%s" ' % (ang,ang,abs(xpos),abs(ypos),obj_str)
+    im_cmd_append = im_cmd_append.replace('XSIGN',xsign)
+    im_cmd_append = im_cmd_append.replace('YSIGN',ysign)
+    return im_cmd_append
+    
+    #convert testlog.png -fill red -annotate %fx%f%s%f%s%f "%i" testannotate.png
+    # % (ang,ang,xsign,xpos,ysign,ypos,det_index)
+    
+def _create_verification_images(detection_table,object_table,link_table):
+     
+    #convert ap_000017.fits -fx "log(abs((u.r+u.g+u.b)*80))" testlog.png
+    
+    # get the detection table from the 5th image slice
+    current_img_detections = detection_table[detection_table.z_index==5]
+    # join it with the detection_object link table
+    current_link = link_table.join(current_img_detections.z_index,on='det_id',how='right')
+        
+    # join it with the object stats table
+    current_objects=current_link.join(object_table,on='obj_id',how='left')
+    # loop through the objects and make a label for each
+    im_cmd = 'convert ap_000005.png -scale 200% -gravity Center'
+    ### here we're centering on each DETECTION and printing a list of each OBJECT that detection is associated with
+    for index,row in current_img_detections.iterrows():
+        matching_objects = current_objects[current_objects['det_id'] == index]
+        obj_list_str = str(list(matching_objects.obj_id)).rstrip(']').lstrip('[')
+        im_cmd+=_overlay_verification_text(obj_list_str,row['x'],row['y'],row['theta'])
+    ### here we're centering on the average position of each OBJECT and printing it at this average position
+    # for index, row in current_objects.iterrows():
+    #     im_cmd+=_overlay_verification_text(str(row['obj_id']),row['x_mean'],row['y_mean'],0)
+    im_cmd += 'detections_000005.png'
+    os.system(im_cmd)
+    
+    #convert testlog.png -fill red -annotate %fx%f%s%f%s%f "10" testannotate.png
+    # % (ang,ang,xsign,xpos,ysign,ypos)
 def make_sex_cat(image_name, checkimages=False):
     '''Create the Source EXtraction CATalog'''
     basename = image_name.split('.')[0]
@@ -181,6 +251,14 @@ def get_dat_ass(det,rad=3.0,slice_rad=10.0):
     detections_accounted_for_set = set()
     pot_obj_list = []
     
+    z_mean_list = []
+    z_std_list = []
+    x_mean_list = []
+    x_std_list = []
+    y_mean_list = []
+    y_std_list = []
+    det_list_list = []
+    
     # get database of associations
     for index,row in det.iterrows():
         current_x = row['x']
@@ -203,15 +281,18 @@ def get_dat_ass(det,rad=3.0,slice_rad=10.0):
         
         potential_object_frame = det[boolean_array]  
         
-        # check if all associated items have already been accoutned for:
+        
         if not len(potential_object_frame.index) >= 3: # only include if there are at least 3 frames within the search radius at this position
             print "too few detections for index %s" % (str(index))
             print potential_object_frame.index
+        # check if all associated items have already been accoutned for by seeing if all detections
+        # in the object frame are already in the detections_accounted_for_set
         if not set(potential_object_frame.index).issubset(detections_accounted_for_set):
             detections_accounted_for_set.update(set(potential_object_frame.index))
             pot_obj_list.append(potential_object_frame)
             
         
+
         # get data frame at specific location (using labels)
         #.loc is strictly label based, will raise KeyError when the items are not found
         # det.loc[[1091,1092,1093]] # returns values based on this index
@@ -219,8 +300,40 @@ def get_dat_ass(det,rad=3.0,slice_rad=10.0):
         # .iloc is strictly integer position based (from 0 to length-1 of the axis), 
         # will raise IndexError when the requested indicies are out of bounds.
         # det.loc[[1091,1092,1093]] # returns values based on this index
-  
-    return pot_obj_list
+        
+    # loop again through object list and populate the object_table and detection_object_link_table?
+    # may be a more efficient way to do this.. 
+    # 
+    object_names = ['z_mean','z_std','x_mean','x_std','y_mean','y_std']
+    
+    count = 0
+    for obj_df in pot_obj_list:
+        means = obj_df.mean()
+        stds = obj_df.std()
+        ind_obj_list = [means['z_index'],stds['z_index'],means['x'],stds['x'],means['y'],stds['y']]
+        tmp_table = pd.DataFrame([ind_obj_list],columns=object_names)
+        obj_det_link_dict = {'obj_id':count,'det_id':obj_df.index}
+        tmp_table2 = pd.DataFrame(obj_det_link_dict)
+        if count == 0:
+            object_table = tmp_table
+            detection_object_link_table = tmp_table2
+        else:
+            object_table = object_table.append(tmp_table,ignore_index=True)
+            detection_object_link_table = detection_object_link_table.append(tmp_table2,ignore_index=True)
+        count += 1
+    
+    #     catid = int(cat_text_file[3:].lstrip('0').rstrip('.txt'))
+    #     tmp_table=pd.read_csv(cat_text_file,header=None,delim_whitespace=True,names=detection_names)
+    #     tmp_table.z_index = catid
+    #     if count == 0:
+    #         detection_table = tmp_table # do this for the first item, then append on to it
+    #     else:
+    #         detection_table = detection_table.append(tmp_table,ignore_index=True)
+    #     count += 1
+    # print str(count) + ' tables read in'
+    
+    
+    return object_table,detection_object_link_table 
         
 def test_pandas():
     import pandas as pd
