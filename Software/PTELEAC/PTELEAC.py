dd2390e45d16b09528dbe19e5ba414680ff9a0c8
Adam N. Morgan <qmorgan@gmail.com>
2014-04-01 13:24:30 -0700

    Added metadata for the events, including plotting bounds

--- Software/PTELEAC/PTELEAC.py
+++ Software/PTELEAC/PTELEAC.py
@@ -14,43 +14,101 @@ import glob
 
 meta_dict = {'061126':{
                         'z':None,
-                        'zlim':1.1588
+                        'zlim':1.1588,
+                        'z':2.346,
+                        'zlim':None,
+                        'xmin':100,
+                        'xmax':2e3,
+                        'ymin':100,
+                        'ymax':3e4
                         },
             '080310': {
                         'z':2.4266,
-                        'zlim':None
+                        'zlim':None,
+                        'z':2.346,
+                        'zlim':None,
+                        'xmin':1.5e3,
+                        'xmax':3e3,
+                        'ymin':6e2,
+                        'ymax':2e3
                         },
             '080330':{
                        'z': 1.51,
-                       'zlim':None
+                       'zlim':None,
+                       'z':2.346,
+                       'zlim':None,
+                       'xmin':1000,
+                       'xmax':2e4,
+                       'ymin':40,
+                       'ymax':2e3
                        },
             '090618':{
                        'z':0.54,
-                       'zlim':None
+                       'zlim':None,
+                       'z':2.346,
+                       'zlim':None,
+                       'xmin':100,
+                       'xmax':3e3,
+                       'ymin':1000,
+                       'ymax':3e4
                        },
             '080319C':{
                         'z':1.95,
-                        'zlim':None
+                        'zlim':None,
+                        'z':2.346,
+                        'zlim':None,
+                        'xmin':300,
+                        'xmax':2e3,
+                        'ymin':100,
+                        'ymax':5e3
                         },
             '090530':{
                        'z':None,
-                       'zlim':1.6
+                       'zlim':1.6,
+                       'z':2.346,
+                       'zlim':None,
+                       'xmin':100,
+                       'xmax':5e3,
+                       'ymin':50,
+                       'ymax':8e3
                        },
             '080607':{
                        'z':3.036,
-                       'zlim':None
+                       'zlim':None,
+                       'z':2.346,
+                       'zlim':None,
+                       'xmin':100,
+                       'xmax':5e3,
+                       'ymin':30,
+                       'ymax':3e4
                        },
             '051109A':{
                         'z':2.346,
-                        'zlim':None
+                        'zlim':None,
+                        'xmin':100,
+                        'xmax':5e3,
+                        'ymin':100,
+                        'ymax':1e4
                         },
             '070208':{
                        'z':1.165,
-                       'zlim':None
+                       'zlim':None,
+                       'z':2.346,
+                       'zlim':None,
+                       'xmin':300,
+                       'xmax':2e4,
+                       'ymin':50,
+                       'ymax':1e3
                        },
             '071025':{
                        'z':4.8,
-                       'zlim':None
+                       'zlim':None,
+                       'z':2.346,
+                       'zlim':None,
+                       'xmin':100,
+                       'xmax':5e3,
+                       'ymin':40,
+                       'ymax':2e4
                        }
         }
 
@@ -81,8 +139,15 @@ def makedict(df_grb, outputtext=False,
     GRB_list = list(df_grb.index)
     
     for index, GRB in enumerate(GRB_list):
+        
         globstr = very_good_pickle_path + GRB + '*'
-        pathstr = glob.glob(globstr)[0]
+        globresult = glob.glob(globstr)
+        if len (globresult) == 0:
+            print "Can't Find any files in {}. External HD plugged in?".format(globresult)
+            raise(ValueError)
+        pathstr = globresult[0]
+        if not os.path.exists(pathstr):
+            print "The path {} doesn't appear to exist. Wrong path?".format(pathstr)
         print pathstr
         result = qPickle.load(pathstr)
         galebv=df_grb.loc[GRB]['galebv']
@@ -94,16 +159,24 @@ def makedict(df_grb, outputtext=False,
     return all_GRB_dict
     
 # MAKE PARSEABLE PHOTOMETRY FILES
-outdict = makedict(df_grb, outputtext=True)
-
+def make_outdict():
+    outdict = makedict(df_grb, outputtext=True)
+    return outdict
+    
 # TRANSPORT TABLES 
 #todo
 
 # load tables
 catdir = "/Users/amorgan/qsoft/store/ptelcat/"
 phottables=glob.glob(catdir+"*data.txt")
-for table in phottables:
-    phot_obj=PhotParse.PhotParse(table)
-    phot_obj.PlotLC(show=False,legend=False)
-    phot_obj.WriteTable()
+
+def obj_loop():
+    for table in phottables:
+        phot_obj=PhotParse.PhotParse(table)
+        
+        GRB_key = phot_obj.name.split(',')[-1] # eg 071025
+        GRB_df = df_grb.loc['GRB_key']
+        
+        phot_obj.PlotLC(show=False,legend=False,xlimits=(100,20000))
+        phot_obj.WriteCondensedTable()
     
