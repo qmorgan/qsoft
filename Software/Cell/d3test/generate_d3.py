7d626f0e5f7e8e1b96affdf596c77b0eeabe9670
Adam N. Morgan <qmorgan@gmail.com>
2014-04-05 05:15:06 -0700

    Creating converter from Migration code output files to d3 format

---
+++ Software/Cell/d3test/generate_d3.py
@@ -0,0 +1,31 @@
+import glob
+import os
+import pandas as pd
+import numpy as np
+
+df=pd.read_csv("/Users/amorgan/qsoft/Software/Cell/d3test/Detections.csv")
+
+columnlist = ['id','x','y','r_kron']  
+
+data_str = '['
+for ind in np.arange(int(df.describe().z_index.loc['max'])):
+    subdf = df[df.z_index == ind]
+    if len(subdf) == 0:
+        data_str += '[],\n' # no data for this slice
+    else:
+        #define formatters to print
+        id_fmt  = lambda x: '\t{id:%i,' % x
+        x_fmt = lambda x: 'x:%.2f,' % x
+        y_fmt = lambda x: 'y:%.2f,' % x
+        r_fmt = lambda x: 'r:%.2f},' % x
+    
+        formatterdict = {'id':id_fmt,'x':x_fmt,'y':y_fmt,'r_kron':r_fmt}
+    
+    
+        out_str = subdf.to_string(index=False,index_names=False,
+                header=False,columns=columnlist,formatters=formatterdict)
+            
+        out_str = '[' + out_str.rstrip(',') + '],\n'# get rid of trailing comma
+        data_str += out_str
+
+data_str = data_str.rstrip(',')+']'
